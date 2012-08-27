6c7280cbfe37b6e59a1da78da7701cc28b5e7de1
Travis McDonald <travismcdonald@gmail.com>
2012-08-27 05:31:06 -0500

    changed user login info

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => 'First User', :email => 'travis@travismcdonald.com', :password => 'test', :password_confirmation => 'please'
 user.confirm!
 puts 'New user created: ' << user.name
 user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
